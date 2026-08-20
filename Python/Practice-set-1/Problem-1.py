09a7f95c3061bbc712662049ddda989f0c9e3ae7
priya-bhagat01 <pb337205@gmail.com>
2026-08-20 21:16:50 +0530

    Added Python practice set

---
+++ Python/Practice-set-1/Problem-1.py
@@ -0,0 +1,11 @@
+# Write program to print Twinkle Twinkle Little Star poem
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are!")
+print("Up above the world so high,")
+print("Like a diamond in the sky.")
+
+#OR
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.""")
