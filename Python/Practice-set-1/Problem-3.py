cfd435e145a6a9a1f45c066c657baa4ec853522f
priya-bhagat01 <pb337205@gmail.com>
2026-08-20 21:25:37 +0530

    Imported pyTTx and ran engine

---
+++ Python/Practice-set-1/Problem-3.py
@@ -0,0 +1,5 @@
+#Install external module and use it to perform operation of your interest
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hi I am a text to speech conversion program. I can read out any text you want me to read.")
+engine.runAndWait()
