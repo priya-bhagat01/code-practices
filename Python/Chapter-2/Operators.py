14e6772e3f98f37ea03b43dd2831cb15071f5ee5
priya-bhagat01 <pb337205@gmail.com>
2026-08-21 17:29:12 +0530

    learned about operators

---
+++ Python/Chapter-2/Operators.py
@@ -0,0 +1,26 @@
+#Arithematic operators are used to perform mathematical operations like addition, subtraction, multiplication, etc.
+a = 10
+b = 5
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  # Division
+
+#Assignment operators are used to assign values to variables. The most common assignment operator is the equal sign (=).
+x = 5
+print(x)  # Output: 5
+
+#Comparison operators are used to compare two values. They return a boolean value (True or False) based on the comparison.
+x = 10
+y = 5
+print(x > y)  # Greater than
+print(x < y)  # Less than
+print(x == y)  # Equal to
+print(x != y)  # Not equal to
+
+#Logical operators are used to combine conditional statements. The most common logical operators are "and", "or", and "not".
+x = True
+y = False
+print(x and y)  # Logical AND
+print(x or y)   # Logical OR
+print(not x)    # Logical NOT
