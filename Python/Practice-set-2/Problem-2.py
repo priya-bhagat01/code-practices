37230c49c03d54c2af76e83c3b3a976d385e185c
priya-bhagat01 <pb337205@gmail.com>
2026-08-21 17:52:31 +0530

    Add program to calculate remainder when a number is divided by 2

---
+++ Python/Practice-set-2/Problem-2.py
@@ -0,0 +1,9 @@
+#Write program to find reminder when a number is divided by 2
+a = 9
+reminder = a % 2
+print(reminder)
+
+#Taking input from user
+a = int(input("Enter a number: "))
+reminder = a % 2
+print("The remainder is:", reminder)
