693ba7aac06c40136a14594162cb4cdb594b9010
priya-bhagat01 <pb337205@gmail.com>
2026-08-21 18:11:17 +0530

    Add user input functionality to compare two numbers

--- Python/Practice-set-2/Problem-4.py
+++ Python/Practice-set-2/Problem-4.py
@@ -3,3 +3,9 @@ a = 34
 b = 80
 c = a > b
 print(c)
+
+#Take input from user and compare if given number a is greater than b or not
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = a > b
+print("a is greater than b is:", c)
