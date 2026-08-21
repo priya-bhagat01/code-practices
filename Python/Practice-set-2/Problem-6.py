1eb7a7702ea22da2d3081439d02f039bae11ce89
priya-bhagat01 <pb337205@gmail.com>
2026-08-21 18:07:25 +0530

    Add program to calculate the square of a number entered by the user

---
+++ Python/Practice-set-2/Problem-6.py
@@ -0,0 +1,4 @@
+#Write python program to calculate sqare of a number entered by user
+a = int(input("Enter a number: "))
+b = a ** 2 #This is called exponentiation operator
+print("The square of the number is:", b)
