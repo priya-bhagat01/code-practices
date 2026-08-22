fc0c6265ab0f783012e1c417bffbb5d401b38951
priya-bhagat01 <pb337205@gmail.com>
2026-08-22 13:18:36 +0530

    Add program to detect and replace double spaces in a string

---
+++ Python/Practice-set-3/Problem-3&4.py
@@ -0,0 +1,7 @@
+#Write program to detect double spaces in a string
+name = "Priya is a good  girl  and she is a good  student"
+print(name.find("  "))
+
+#Write program to replace double spaces with single space
+name = name.replace("  ", " ")
+print(name)
