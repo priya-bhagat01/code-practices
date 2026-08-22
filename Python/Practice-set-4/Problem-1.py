257f28984239d304c0dfde1a59d4b4b09007203c
priya-bhagat01 <pb337205@gmail.com>
2026-08-22 16:57:48 +0530

    Add program to store and display a list of 7 user-entered fruits

---
+++ Python/Practice-set-4/Problem-1.py
@@ -0,0 +1,26 @@
+#Write program to store 7 fruits in a list entered by the user. Display the fruits in the list.
+# Initialize an empty list to store the fruits
+fruits = []
+
+f1 = input("Enter the name of fruit 1: ")
+fruits.append(f1)
+
+f2 = input("Enter the name of fruit 2: ")
+fruits.append(f2)
+
+f3 = input("Enter the name of fruit 3: ")
+fruits.append(f3)
+
+f4 = input("Enter the name of fruit 4: ")
+fruits.append(f4)
+
+f5 = input("Enter the name of fruit 5: ")
+fruits.append(f5)
+
+f6 = input("Enter the name of fruit 6: ")
+fruits.append(f6)
+
+f7 = input("Enter the name of fruit 7: ")
+fruits.append(f7)
+
+print("The fruits in the list are:", fruits)
