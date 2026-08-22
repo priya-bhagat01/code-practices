df2baecdf6f5624ca2a18c4287caee038df92fc1
priya-bhagat01 <pb337205@gmail.com>
2026-08-22 17:10:21 +0530

    Add program to sum four user-entered numbers

---
+++ Python/Practice-set-4/Problem-4.py
@@ -0,0 +1,6 @@
+#Write program to sum list with 4 numbers entered by the user. 
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+c = int(input("Enter number 3: "))
+d = int(input("Enter number 4: "))
+print("The sum of the numbers is:", a + b + c + d)
