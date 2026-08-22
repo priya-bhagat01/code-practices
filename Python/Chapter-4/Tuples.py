5a0a9c142b15a0ab8067d8da032c80e660b9c5d2
priya-bhagat01 <pb337205@gmail.com>
2026-08-22 16:08:40 +0530

    Add detailed examples of tuple operations and methods in Python

--- Python/Chapter-4/Tuples.py
+++ Python/Chapter-4/Tuples.py
@@ -9,3 +9,25 @@ c = (1, 2, 3, 4, 5) #Tuple with multiple elements
 a = (1, 2, 3, 4, 5)
 print(a.count(2))  # Output: 1, counts the number of occurrences of the specified value in the tuple
 print(a.index(3))  # Output: 2, returns the index of the first occurrence of the specified value in the tuple
+
+#Operations on Tuples
+a = (1, 2, 3)
+b = (4, 5, 6)
+c = a + b  # Concatenation of tuples
+print(c)  # Output: (1, 2, 3, 4, 5, 6)
+
+d = a * 2  # Repetition of tuples
+print(d)  # Output: (1, 2, 3, 1, 2, 3)
+
+print(2 in a)  # Output: True, checks if the value is present in the tuple
+
+print(len(a))  # Output: 3, returns the number of elements in the tuple
+
+print(min(a))  # Output: 1, returns the smallest element in the tuple
+print(max(a))  # Output: 3, returns the largest element in the tuple
+
+sliced = a[1:3]  # Slicing the tuple
+print(sliced)  # Output: (2, 3), returns a new tuple containing the elements from index 1 to index 2 (3 is not included)
+
+d , b , c = a  # Tuple unpacking
+print(d , b , c)  # Output: 1 2 3
