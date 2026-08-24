b36c73a3e56ff5d22532d2dffab24ea18df3f59c
priya-bhagat01 <pb337205@gmail.com>
2026-08-24 21:35:29 +0530

    Added another way to print sum

--- Python/Practice-set-4/Problem-4.py
+++ Python/Practice-set-4/Problem-4.py
@@ -4,3 +4,7 @@ b = int(input("Enter number 2: "))
 c = int(input("Enter number 3: "))
 d = int(input("Enter number 4: "))
 print("The sum of the numbers is:", a + b + c + d)
+
+#Another way to do so
+l = [1, 2, 3, 4]
+print(sum(l))
