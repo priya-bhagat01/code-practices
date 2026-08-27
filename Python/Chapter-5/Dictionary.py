f910fe0decc7232119f39b3e1e90b12fe6e1e9dc
priya-bhagat01 <pb337205@gmail.com>
2026-08-27 18:48:57 +0530

    Learned a few more methods

--- Python/Chapter-5/Dictionary.py
+++ Python/Chapter-5/Dictionary.py
@@ -20,3 +20,27 @@ print(marks)
 
 # .get returns value of specified key
 print(marks.get("Priya"))
+
+#Difference between marks.get and marks[] is 
+#When marks.get key doesn't exist it gives none
+print(marks.get("Siya"))
+
+#When marks[] key doesn't exist it gives error
+#print(marks["Siya"])
+
+# .copy copies the dictionaries 
+# .clear clears everything
+#print(marks.clear())
+
+# .fromkeys creates new dictionary 
+keys = ['a', 'b', 'c']
+new_dict = dict.fromkeys(keys, 0)
+print(new_dict) #Takes keys a, b, c with values 0
+
+# .pop removes specified key and returns value, if not found default or keyerror is raised
+value = marks.pop("Priya", 'default_value')
+print(value)
+
+# .popitem removes and returns key value pair, returnd in (last-in, first-out) order
+item = marks.popitem()
+print(item)
