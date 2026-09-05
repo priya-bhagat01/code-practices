f9b64447336b5f555058cd0c055e10af0b5ffcca
priya-bhagat01 <pb337205@gmail.com>
2026-09-05 22:15:20 +0530

    What happens if two languages are same

---
+++ Python/Practice-set-5/Problem8.py
@@ -0,0 +1,16 @@
+#If languages of two friends are same, what will happen to program in problem 6
+
+fav_language = {}
+fav_language.update({input("Enter your name:"): input("Enter your favourite language:")})
+fav_language.update({input("Enter your name:"): input("Enter your favourite language:")})
+fav_language.update({input("Enter your name:"): input("Enter your favourite language:")})
+fav_language.update({input("Enter your name:"): input("Enter your favourite language:")})
+print(fav_language)
+
+#Nothing evident happens two values get assigned to different names
+#fav_language = {
+# "Priya": "Python",
+# "Ashu": "Python",
+# "Shrey": "JavaScript",
+# "Arya": "C++"
+#}
