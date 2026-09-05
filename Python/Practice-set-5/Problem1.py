6145b2b2d7a116511a8a3fb994bf8056776378e8
priya-bhagat01 <pb337205@gmail.com>
2026-09-05 19:35:40 +0530

    Hindi words dictionary

---
+++ Python/Practice-set-5/Problem1.py
@@ -0,0 +1,12 @@
+#Program to create dictionary of Hindi words with values as English translation 
+
+words = {
+    "bhediya": "Wolf",
+    "madad": "help",
+    "kursi": "chair",
+    "billi": "cat",
+    "kutta": "dog"
+}
+
+word = input("Enter the word you want to translate:")
+print(words[word]) #word is variable when it takes input from user words[word] find the key value pair
