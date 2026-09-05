ef3e34a7be56ffdad6e97940001fbe3e53aa139d
priya-bhagat01 <pb337205@gmail.com>
2026-09-05 19:45:27 +0530

    Taking input from user and displaying altogether

---
+++ Python/Practice-set-5/Problem2.py
@@ -0,0 +1,13 @@
+#Program to input eight numbers from user and display all unique numbers once
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+c = int(input("Enter number 3: "))
+d = int(input("Enter number 4: "))
+e = int(input("Enter number 5: "))
+f = int(input("Enter number 6: "))
+g = int(input("Enter number 7: "))
+h = int(input("Enter number 8: "))
+
+set = {a, b, c, d, e, f, g, h}
+print(set)
