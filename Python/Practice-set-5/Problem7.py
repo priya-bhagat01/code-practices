84096f0afb378d4ebbf6d68c828d54bd98c0cbf3
priya-bhagat01 <pb337205@gmail.com>
2026-09-05 21:27:59 +0530

    What happens if two names are same

---
+++ Python/Practice-set-5/Problem7.py
@@ -0,0 +1,19 @@
+#If names of 2 friends are same, what will happen in problem 6
+
+fav_language = {}
+fav_language.update({input("Enter your name:"): input("Enter your favourite language:")})
+fav_language.update({input("Enter your name:"): input("Enter your favourite language:")})
+fav_language.update({input("Enter your name:"): input("Enter your favourite language:")})
+fav_language.update({input("Enter your name:"): input("Enter your favourite language:")})
+print(fav_language)
+
+#The next value is applied to the name and only 3 key value pairs exist (depends on how many same)
+#ex: Priya: JavaScript
+#Priya: Python
+#Ashu: C++
+#Saina: Java
+#fav_language = {
+#   "Priya": "Python",
+#   "Ashu": "C++",
+#   "Saina": "Java"
+#}
