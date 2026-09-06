bf82168563711824102b77d2274741bde8918327
priya-bhagat01 <pb337205@gmail.com>
2026-09-06 13:06:59 +0530

    If else elif condtions

---
+++ Python/Chapter-6/Conditionals.py
@@ -0,0 +1,18 @@
+#IF ELSE & ELIF in python are multiway decision taken by our program 
+#due to certain conditions in our code
+#Syntax:
+condition1 = 0
+condition2 = 0
+if(condition1): #if condition1 is true
+    print("yes")
+elif(condition2): #if condition2 is true
+    print("no")
+else:               #otherwise
+    print("maybe")
+
+#Code example:
+a = 22
+if(a > 9):
+    print("greater")
+else: 
+    print("lesser")
