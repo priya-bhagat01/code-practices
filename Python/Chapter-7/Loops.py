f845eaef08d182d49d241506e7de643a15706f6d
priya-bhagat01 <pb337205@gmail.com>
2026-09-06 18:59:01 +0530

    Write 1 to 5 using while and for loop

---
+++ Python/Chapter-7/Loops.py
@@ -0,0 +1,16 @@
+#Write 1 to 5
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+#Using while loop
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+#Using for loop
+for i in range(1, 6):
+    print(i)
