7b46dd159d64463235d4aebf19d6ebb7a5ed400c
priya-bhagat01 <pb337205@gmail.com>
2026-09-06 15:17:00 +0530

    Find greatest of four numbers

---
+++ Python/Practice-set-6/Problem1.py
@@ -0,0 +1,20 @@
+#Program to find greatest of four numbers entered by user
+a = int(input("Enter number: "))
+b = int(input("Enter number: "))
+c = int(input("Enter number: "))
+d = int(input("Enter number: "))
+
+if(a > b and a > c and a > d ):
+    print("Greatest number is a:", a)
+
+if(b > a and b > c and b > d ):
+    print("Greatest number is b:", b)
+
+if(c > a and c > b and c > d ):
+    print("Greatest number is c:", c)
+
+if(d > a and d > b and d > c ):
+    print("Greatest number is d:", d)
+
+if(a == b == c == d):
+    print("Greatest number is:", a)
