2f1d27adecfb7225c1c071ffb130815c66ada3dc
priya-bhagat01 <pb337205@gmail.com>
2026-09-08 21:08:12 +0530

    Fixed new line problem

--- Python/Practice-set-7/Problem7.py
+++ Python/Practice-set-7/Problem7.py
@@ -10,4 +10,4 @@ n = int(input("Enter number: "))
 for i in range(1, n+1):
     print(" "* (n-i), end="")
     print("*"* (2*i - 1), end="") #end= don't add new line by default
-    print("\n")
+    print("")
