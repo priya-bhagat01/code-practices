021fbcb42c1e8be5255c98371393cec47488251a
priya-bhagat01 <pb337205@gmail.com>
2026-09-08 21:06:50 +0530

    Printing star pattern-1

---
+++ Python/Practice-set-7/Problem7.py
@@ -0,0 +1,13 @@
+#Program to print following star pattern
+'''
+  *
+ ***
+***** for n = 3
+'''
+
+n = int(input("Enter number: "))
+
+for i in range(1, n+1):
+    print(" "* (n-i), end="")
+    print("*"* (2*i - 1), end="") #end= don't add new line by default
+    print("\n")
