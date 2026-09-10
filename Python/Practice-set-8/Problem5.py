06f84cb821b0e98415242679ea1cf7145d26bf50
priya-bhagat01 <pb337205@gmail.com>
2026-09-10 21:14:21 +0530

    Solved with recursion method

--- Python/Practice-set-8/Problem5.py
+++ Python/Practice-set-8/Problem5.py
@@ -13,3 +13,11 @@ def starPattern(i, n):
         print("*"* (n - i))
         i += 1
 starPattern(i, n)
+
+def pattern(n):
+    if n == 0:
+        # print("")
+        return
+    print("*"* n)
+    pattern(n - 1)
+pattern(n)
