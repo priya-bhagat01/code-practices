daf95e2a14ec80dbf3b38712a7e33d2e034acfec
priya-bhagat01 <pb337205@gmail.com>
2026-09-10 17:01:34 +0530

    Function syntax and definition

---
+++ Python/Chapter-8/Functions.py
@@ -0,0 +1,16 @@
+#function is group of statements performing specific tasks
+#When program gets bigger in size & its complexity grows, it gets difficult for program 
+#to keep track on which piece of code is doing  what
+#Function can be reused by programmer in given program
+
+'''
+Syntax:
+def func1():
+    print("hello")
+func1() #this is called function call
+'''
+
+#Program to greet user with "Good Day" using function
+def greet():
+    print("Good Day")
+greet() 
