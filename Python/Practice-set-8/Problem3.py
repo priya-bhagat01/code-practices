cd9b58b140aad72b0060ec9914e6d2725b91adda
priya-bhagat01 <pb337205@gmail.com>
2026-09-10 18:41:42 +0530

    Preventing print function to prevent new line at end

---
+++ Python/Practice-set-8/Problem3.py
@@ -0,0 +1,8 @@
+#How do you prevent python print() function to prevent new line at the end
+print("Hello")
+print("Priya")
+#prints Hello
+        #Priya
+print("Hello, ", end="")
+print("Priya")
+#prints Hello, Priya
