35abc9575bf0c10e29be9ab759cdffad91b70533
priya-bhagat01 <pb337205@gmail.com>
2026-09-10 21:27:17 +0530

    Solved with loop in function

--- Python/Practice-set-8/Problem7.py
+++ Python/Practice-set-8/Problem7.py
@@ -11,3 +11,11 @@ def removeWord(n):
         print(f"{n} not found in list")
 
 removeWord(n)
+
+def rem(l, word):
+    for item in l:
+        l.remove(word)
+        return l
+
+l = ["Harry", "Rohan", "Shubham", "an"]
+print(rem(l, "an"))
