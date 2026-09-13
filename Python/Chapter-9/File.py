28dfa69457bd079a2c3d81cf5c0258f1d1ba7578
priya-bhagat01 <pb337205@gmail.com>
2026-09-13 17:50:14 +0530

    Files definition and types of files

---
+++ Python/Chapter-9/File.py
@@ -0,0 +1,31 @@
+#RAM - random access memory is volatile & all its contents are lost once program terminates 
+#In order to persist data forever, we use files
+
+#File is data stored in storage device
+#Python program can talk to file by reading & writing content from it
+
+#Type of files:
+#Text files (.txt, .c, etc)
+#Binary files (.jpg, .dat, etc)
+#Python has lot of function for reading, updating & deleted files
+
+#Opening file
+#Python has open() function for opening files
+#It takes 2 parameters, filename & mode
+#open("filename", "mode of opening(read mode)")
+#open("this txt", "v")
+
+#Reading file in python
+
+#Open file in read mode 
+#f = open("this.txt", "r")
+
+#Read its content
+#text = f.read()
+
+#Print its content 
+#print(text)
+
+#Close the file
+#f.close()
+
