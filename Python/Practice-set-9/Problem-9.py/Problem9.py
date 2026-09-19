4ab2dee9c4e5fa47a2f910c58b1c16ef1490911e
priya-bhagat01 <pb337205@gmail.com>
2026-09-19 14:59:24 +0530

    Find out if files is identical

---
+++ Python/Practice-set-9/Problem-9.py/Problem9.py
@@ -0,0 +1,13 @@
+#Program to find out whether file is identical & matches content of another file
+
+with open("Problem-9.py/File1.txt") as f:
+    content1 = f.read()
+
+with open("Problem-9.py/File2.txt") as f:
+    content2 = f.read()
+
+if content1 == content2:
+    print("Yes these files are identical")
+
+else:
+    print("Yes these files are identical")
